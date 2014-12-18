2ca3513485bbc77940b7a39b68a5b982c371d51b
daisy1754 <d-part@hotmail.co.jp>
2014-12-17 23:55:59 -0800

    Initial commit

---
+++ library/src/main/java/jp/gr/java_conf/daisy/infinite_scroll_helper/InfiniteScrollHelper.java
@@ -0,0 +1,66 @@
+package jp.gr.java_conf.daisy.infinite_scroll_helper;
+
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.ListView;
+
+/**
+ * Helper class for infinite-scroll like behavior. Note that this class use
+ * {@link android.widget.ListView#setOnScrollListener(android.widget.AbsListView.OnScrollListener)}
+ * and {@link android.widget.ListView#addFooterView(android.view.View)}.
+ */
+public class InfiniteScrollHelper {
+    public enum LoadingState {
+        INITIAL, PARTIALLY_LOADED, LOADING, COMPLETE_LOADED, ERROR
+    }
+
+    public interface ListItemFetcher {
+        public boolean canFetchMoreItems();
+        public void fetchMoreItems();
+    }
+
+    private LoadingState mLoadingState = LoadingState.INITIAL;
+    private final View mLoadingIndicatorView;
+    private final ListView mListView;
+
+    public InfiniteScrollHelper(
+            ListView listView, final ListItemFetcher listItemFetcher, View loadingIndicatorView) {
+        mListView = listView;
+        mLoadingIndicatorView = loadingIndicatorView;
+        reset();
+
+        mListView.addFooterView(mLoadingIndicatorView);
+        mListView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
+                                 int totalItemCount) {
+                synchronized (this) {
+                    if (firstVisibleItem + visibleItemCount == mListView.getCount()
+                            && mLoadingState == LoadingState.PARTIALLY_LOADED
+                            && listItemFetcher.canFetchMoreItems()) {
+                        mLoadingState = LoadingState.LOADING;
+                        listItemFetcher.fetchMoreItems();
+                        mLoadingIndicatorView.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        });
+    }
+
+    public void notifyLoadingCompletion(LoadingState state) {
+        mLoadingState = state;
+        mLoadingIndicatorView.setVisibility(View.GONE);
+        if (state == LoadingState.COMPLETE_LOADED) {
+            mListView.removeFooterView(mLoadingIndicatorView);
+        }
+    }
+
+    public void reset() {
+        mLoadingState = LoadingState.INITIAL;
+        mLoadingIndicatorView.setVisibility(View.GONE);
+    }
+}
---
+++ sample/src/main/java/jp/gr/java_conf/daisy/infinite_scroll_helper/sample/MainActivity.java
@@ -0,0 +1,79 @@
+package jp.gr.java_conf.daisy.infinite_scroll_helper.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import jp.gr.java_conf.daisy.infinite_scroll_helper.InfiniteScrollHelper;
+
+public class MainActivity extends Activity {
+
+    private ListView mListView;
+    private ArrayAdapter<String> mAdapter;
+    private InfiniteScrollHelper mInfiniteScrollHelper;
+    private Handler mHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mListView = (ListView) findViewById(R.id.listView);
+        mAdapter = new ArrayAdapter<String>(this, R.layout.list_item);
+        mListView.setAdapter(mAdapter);
+        FakeListItemFetcher fetcher = new FakeListItemFetcher();
+        mInfiniteScrollHelper = new InfiniteScrollHelper(
+                mListView, fetcher, new ProgressBar(this));
+        mAdapter.addAll(fetcher.initialItems());
+        mInfiniteScrollHelper.notifyLoadingCompletion(
+                InfiniteScrollHelper.LoadingState.PARTIALLY_LOADED);
+        mHandler = new Handler();
+    }
+
+    private class FakeListItemFetcher implements InfiniteScrollHelper.ListItemFetcher {
+        private final int PAGE_SIZE = 10;
+        private final List<String> FAKE_DATA = new ArrayList<String>(Arrays.asList(new String[] {
+                "Cat", "Dog", "Lion", "Elephant", "Monkey", "Sheep", "Tiger", "Zebra", "Giraffe",
+                "Cow", "Horse", "Human", "Rhino", "Bear", "Deer", "Chimpanzee", "Hedgehog",
+                "Mouse", "Rabbit", "Turtle", "Boar", "Panda", "Bird", "Fox", "Wolf"
+        }));
+        private int loadedDataIndex;
+
+        private List<String> initialItems() {
+            loadedDataIndex = PAGE_SIZE;
+            return FAKE_DATA.subList(0, loadedDataIndex);
+        }
+
+        @Override
+        public boolean canFetchMoreItems() {
+            return loadedDataIndex < FAKE_DATA.size() - 1;
+        }
+
+        @Override
+        public void fetchMoreItems() {
+            // Simulate network delay.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    int nextIndex = loadedDataIndex + PAGE_SIZE;
+                    if (nextIndex > FAKE_DATA.size()) {
+                        nextIndex = FAKE_DATA.size();
+                    }
+                    mAdapter.addAll(FAKE_DATA.subList(loadedDataIndex, nextIndex));
+                    loadedDataIndex = nextIndex;
+                    mInfiniteScrollHelper.notifyLoadingCompletion(
+                            loadedDataIndex == FAKE_DATA.size()
+                                    ? InfiniteScrollHelper.LoadingState.COMPLETE_LOADED
+                                    : InfiniteScrollHelper.LoadingState.PARTIALLY_LOADED
+                    );
+                }
+            }, 1000);
+        }
+    }
+}
